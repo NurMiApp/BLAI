c778d4cb92f12fb0a59e9c1bed34a5b5031ca986
NurMi App <sasha.rostovskaya@bk.ru>
2024-09-11 18:24:43 +0300

    Create messages_en.py

---
+++ messages_en.py
@@ -0,0 +1,5 @@
+messages = {
+    "welcome": "Welcome to Bumplab.AI News Selector!",
+    "prompt": "Please enter your topic of interest: ",
+    "error": "Sorry, we could not find any articles related to that topic."
+}
